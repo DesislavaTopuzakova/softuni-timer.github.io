0221e99799f689c193e25ad859fec55d649dbd52
Desislava Topuzakova <34713586+DesislavaTopuzakova@users.noreply.github.com>
2023-05-12 15:14:45 +0300

    Update mAY 2023

---
+++ scripts/partners.js
@@ -0,0 +1,14 @@
+export const partners = [
+  { src: "./public/partners/coca-cola.png", name: "Coca Cola HBC" },
+  { src: "./public/partners/draft-kings.png", name: "DraftKings" },
+  { src: "./public/partners/indeaver.png", name: "Indeavr" },
+  { src: "./public/partners/postbank.png", name: "PostBank" },
+  { src: "./public/partners/smartit.png", name: "SmartIt" },
+  { src: "./public/partners/software-group.png", name: "Software Group" },
+  { src: "./public/partners/superhosting.png", name: "SuperHosting" },
+  { src: "./public/partners/pokerstars.png", name: "PokerStars" },
+  { src: "./public/partners/dxc.png", name: "DXC Technology" },
+  { src: "./public/partners/createx.png", name: "CreateX" },
+  { src: "./public/partners/ambitioned.png", name: "Ambitioned" },
+  { src: "./public/partners/bosch.png", name: "Bosch Digital" },
+];
